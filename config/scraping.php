337ea6b499ebaa460ee001a5ce7ab7092a468f0c
Grégory <contact@gregory-gerard.dev>
2021-03-13 12:10:55 +0100

    Supprime les RX 5700

--- config/scraping.php
+++ config/scraping.php
@@ -11,14 +11,6 @@ return [
             'id' => 2,
             'url' => 'https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_nblignes_est_200_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_58-11575.html',
         ],
-        'rx5700' => [
-            'id' => 3,
-            'url' => 'https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_nblignes_est_200_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_58-10586.html',
-        ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_nblignes_est_200_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_58-10587.html',
-        ]
     ],
 
     'materielnet' => [
@@ -30,14 +22,6 @@ return [
             'id' => 2,
             'url' => 'https://www.materiel.net/carte-graphique/l426/+fv121-19365/',
         ],
-        'rx5700' => [
-            'id' => 3,
-            'url' => 'https://www.materiel.net/carte-graphique/l426/+fv121-17714/',
-        ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.materiel.net/carte-graphique/l426/+fv121-17715/',
-        ]
     ],
 
     'ldlc' => [
@@ -49,14 +33,6 @@ return [
             'id' => 2,
             'url' => 'https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fv121-19365.html',
         ],
-        'rx5700' => [
-            'id' => 3,
-            'url' => 'https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fv121-17714.html',
-        ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fv121-17715.html',
-        ]
     ],
 
     'smidistri' => [
@@ -79,10 +55,6 @@ return [
             'id' => 2,
             'url' => 'https://www.cybertek.fr/carte-graphique-6.aspx?crits=4229',
         ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.cybertek.fr/carte-graphique-6.aspx?crits=3991',
-        ]
     ],
 
     'rueducommerce' => [
@@ -94,10 +66,6 @@ return [
             'id' => 2,
             'url' => 'https://www.rueducommerce.fr/listingDyn?urlActuelle=3060-ti&boutique_id=18&langue_id=1&recherche=3060-ti&from=0'
         ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.rueducommerce.fr/listingDyn?boutique_id=18&langue_id=1&categoriesId=15333&caracteristiquesValeursId=473151&from=0'
-        ]
     ],
 
     'alternate' => [
@@ -110,14 +78,6 @@ return [
                 'id' => 2,
                 'url' => 'https://fr.alternate.be/html/listings/1606405688744?size=500&sort=PRICE&order=ASC'
             ],
-            'rx5700' => [
-                'id' => 3,
-                'url' => 'https://fr.alternate.be/html/listings/1562322675146?size=500&sort=PRICE&order=ASC'
-            ],
-            'rx5700xt' => [
-                'id' => 4,
-                'url' => 'https://fr.alternate.be/html/listings/1562322668528?size=500&sort=PRICE&order=ASC'
-            ],
         ],
 
         'alternatede' => [
@@ -129,14 +89,6 @@ return [
                 'id' => 2,
                 'url' => 'https://www.alternate.de/html/listings/1606466588030?size=500&sort=PRICE&order=ASC'
             ],
-            'rx5700' => [
-                'id' => 3,
-                'url' => 'https://www.alternate.de/html/listings/1561620484893?size=500&sort=PRICE&order=ASC'
-            ],
-            'rx5700xt' => [
-                'id' => 4,
-                'url' => 'https://www.alternate.de/html/listings/1561620588772?size=500&sort=PRICE&order=ASC'
-            ],
         ],
 
         'alternatenl' => [
@@ -147,14 +99,6 @@ return [
             'rtx3060ti' => [
                 'id' => 2,
                 'url' => 'https://www.alternate.nl/html/listings/1606466588030?size=500&sort=PRICE&order=ASC'
-            ],
-            'rx5700' => [
-                'id' => 3,
-                'url' => 'https://www.alternate.nl/html/listings/1561620484893?size=500&sort=PRICE&order=ASC'
-            ],
-            'rx5700xt' => [
-                'id' => 4,
-                'url' => 'https://www.alternate.nl/html/listings/1561620588772?size=500&sort=PRICE&order=ASC'
             ],
         ]
     ],
@@ -169,14 +113,6 @@ return [
                 'id' => 2,
                 'url' => 'https://www.artencraft.nl/nl/computer/componenten/videokaarten/?7493=geforce-rtx-3060-ti&filterload=1'
             ],
-            'rx5700' => [
-                'id' => 3,
-                'url' => 'https://www.artencraft.nl/nl/computer/componenten/videokaarten/?7493=radeon-rx-5700&filterload=1'
-            ],
-            'rx5700xt' => [
-                'id' => 4,
-                'url' => 'https://www.artencraft.nl/nl/computer/componenten/videokaarten/?7493=radeon-rx-5700-xt&filterload=1'
-            ],
         ],
         'artencraftbe' => [
             'rtx3080' => [
@@ -186,14 +122,6 @@ return [
             'rtx3060ti' => [
                 'id' => 2,
                 'url' => 'https://www.artencraft.be/fr/ordinateurs/composants/cartes-video/?7493=geforce-rtx-3060-ti&filterload=1'
-            ],
-            'rx5700' => [
-                'id' => 3,
-                'url' => 'https://www.artencraft.be/fr/ordinateurs/composants/cartes-video/?7493=radeon-rx-5700&filterload=1'
-            ],
-            'rx5700xt' => [
-                'id' => 4,
-                'url' => 'https://www.artencraft.be/fr/ordinateurs/composants/cartes-video/?7493=radeon-rx-5700-xt&filterload=1'
             ],
         ],
     ],
@@ -207,20 +135,12 @@ return [
             'id' => 2,
             'url' => 'https://www.caseking.de/pc-komponenten/grafikkarten?sPerPage=48&sTemplate=list&ckFilters=14202&ckTab=0&sPage=1&sSort=3'
         ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.caseking.de/pc-komponenten/grafikkarten?sPerPage=48&sTemplate=list&ckFilters=11876&ckTab=0&sPage=1&sSort=3'
-        ],
     ],
 
     'cloudmarkt' => [
         'rtx3080' => [
             'id' => 1,
             'url' => 'https://www.cloudmarkt.de/search?p=1&q=rtx%203080&o=3&n=50&f=408545'
-        ],
-        'rx5700xt' => [
-            'id' => 4,
-            'url' => 'https://www.cloudmarkt.de/grafikkarten/?p=1&o=3&n=50&f=408545%7C410650%7C408896'
         ],
     ],
 
